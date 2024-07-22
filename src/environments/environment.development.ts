2e39a23ae202c19619ec208f0444b878b10e5787
Saurabh <saurab1311@gmail.com>
2024-07-22 13:43:41 +0530

    ng-chat-ui-setupt

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://sbvvhwvwfoqeczehiawh.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNidnZod3Z3Zm9xZWN6ZWhpYXdoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEzNzYyMDQsImV4cCI6MjAzNjk1MjIwNH0.rKErEkqu7ICG7kaUI1xuwv2fF_ClvnEG79LcPxQFk8A',
+};
